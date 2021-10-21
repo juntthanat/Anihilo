fa632461359a9805dfa25938ffe63c432163c868
Chayut Liewlom <ray.daroonpat.2020@gmail.com>
2021-10-21 22:53:10 +0700

    Feature: Add disconnect popup (#29)

---
+++ Frontend/utility/DisconnectMessage.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+public class DisconnectMessage {
+
+    public static int disconnected() {
+        JFrame disconnect = new JFrame();
+        JOptionPane.showMessageDialog(disconnect, "Internet Unstable or Disconnect, \nPlease Check Your Connection",
+                "ALERT", JOptionPane.WARNING_MESSAGE);
+        if (JOptionPane.OK_OPTION == 0) {
+            return 1;
+        } else
+            return 0;
+    }
+}
