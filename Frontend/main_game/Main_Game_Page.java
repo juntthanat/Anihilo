f3a6df8e995be5a0a9d9da242225f5c0c65814c0
Janthanat Thanintantrakun <beamnut@hotmail.com>
2021-11-02 22:39:49 +0700

    feat: rework main-game

--- Frontend/main_game/Main_Game_Page.java
+++ Frontend/main_game/Main_Game_Page.java
@@ -4,15 +4,18 @@ import Frontend.utility.Score_Changer;
 import Frontend.utility.Instruction_Changer;
 import Frontend.utility.Life_Changer;
 
-import java.awt.CardLayout;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+
+import java.awt.CardLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 public class Main_Game_Page {
     private Instruction instruction;
     private Game_Part game_part;
     private Score_Part score_part;
-    private JPanel main_game;
+    private JPanel main_game, menu;
 
     public Main_Game_Page(JPanel main_page, CardLayout page) {
         main_game = main_Game(main_page, page);
@@ -27,14 +30,15 @@ public class Main_Game_Page {
      * @return JPanel The main game page
      */
     public JPanel main_Game(JPanel main_page, CardLayout page) {
-        JPanel menu = new JPanel();
-        menu.setBounds(0, 0, 750, 600);
-        menu.setLayout(null);
+        menu = new JPanel();
+        menu.setPreferredSize(new Dimension(750, 600));
+        // menu.setBounds(0, 0, 750, 600);
+        menu.setLayout(new BorderLayout());
 
         // * create panel.
         instruction = new Instruction(menu, main_page, page);
-        game_part = new Game_Part(menu, main_page, page);
         score_part = new Score_Part(menu, main_page, page);
+        game_part = new Game_Part(menu, main_page, page);
 
         main_page.add(menu);
 
